c546929987195ad7f82400933540e3a008ff3e69
Avery Lin <tclin100@gmail.com>
2021-09-15 17:43:21 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(number1, number2) {
+    return number1+number2;
+}
+function subtract(number1, number2) {
+    return number1-number2
+}
+function multiply(number1, number2) {
+    return number1*number2
+}
+function divide(number1, number2) {
+    return number1/number2
+}
+function increment(number) {
+    return number+=1;
+}
+function decrement(number) {
+    return number-=1;
+}
+function makeInt(number) {
+    parseInt(number,10);
+    return parseInt(number, 10);
+}
+function preserveDecimal(number) {
+    parseFloat(number, 10);
+    return parseFloat (number, 10);
+}
